4f26071836b57d72d75b7d8044916bc43a385470
Manmadedrummer <140130825+Manmadedrummer@users.noreply.github.com>
2024-08-06 18:51:59 -0400

    Create gameobject_loot_template.sql

---
+++ Add To Server Side/gameobject_loot_template.sql
@@ -0,0 +1,2 @@
+INSERT INTO gameobject_loot_template (Entry,Item,Reference,Chance,QuestRequired,LootMode,GroupId,MinCount,MaxCount,Comment) VALUES
+	 (900900,900900,0,100.0,0,1,0,1,3,'Snowberry Bush Valley of Snow');
